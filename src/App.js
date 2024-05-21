e0e6c1ba167c22588f4db888497a66acf9056539
Aisha <aicha.cs@gmail.com>
2024-05-21 11:21:45 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
+import fleur from './fleur.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Hello World</h2>
+        <img src={fleur} className="App-logo" alt="logo" />
+        <h2>Hello Aisha</h2>
         <p>
-          Welcome All Buddies to Buddy Webinar!!!
+           Bienvenue chez nous, vous êtes chez vous !!!
         </p>
       </header>
     </div>
